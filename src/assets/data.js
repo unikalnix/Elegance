597f4a587f31f576f2d221467cafa0f8654dcd12
Malik <hafizdaniyalshakeel@gmail.com>
2025-03-26 01:20:36 +0500

    images paths fixed

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,35 +4,35 @@ export const categories = [
     _id: "1",
     type: "category",
     title: "Men",
-    image: "/public/images/men.jpg",
+    image: "/images/men.jpg",
     description: "Summer & Winter Collection",
   },
   {
     _id: "2",
     type: "category",
     title: "Women",
-    image: "/public/images/women.jpg",
+    image: "/images/women.jpg",
     description: "Summer & Winter Collection",
   },
   {
     _id: "3",
     type: "category",
     title: "Kids",
-    image: "/public/images/kid.jpeg",
+    image: "/images/kid.jpeg",
     description: "Summer & Winter Collection",
   },
   {
     _id: "4",
     type: "category",
     title: "Accessories",
-    image: "/public/images/accessory.jpg",
+    image: "/images/accessory.jpg",
     description: "Complete Your Look",
   },
   {
     _id: "4",
     type: "category",
     title: "Accessories",
-    image: "/public/images/accessory.jpg",
+    image: "/images/accessory.jpg",
     description: "Complete Your Look",
   },
 ];
@@ -43,7 +43,7 @@ export const exclusiveCollection = [
     title:"Premium Cotton Shirt",
     price:"89.99",
     isNew: true,
-    image:"/public/images/shirt.jpeg"
+    image:"/images/shirt.jpeg"
   },
   {
     _id: "2",
@@ -51,7 +51,7 @@ export const exclusiveCollection = [
     title:"Designer Slim-Fit Jeans",
     price:"129.99",
     isNew: false,
-    image:"/public/images/jeans.jpg"
+    image:"/images/jeans.jpg"
   },
   {
     _id: "3",
@@ -59,7 +59,7 @@ export const exclusiveCollection = [
     title:"Merino Wool Sweater",
     price:"159.99",
     isNew: true,
-    image:"/public/images/sweater.jpeg"
+    image:"/images/sweater.jpeg"
   },
   {
     _id: "4",
@@ -67,7 +67,7 @@ export const exclusiveCollection = [
     title:"Italian Leather Loafers",
     price:"199.99",
     isNew: false,
-    image:"/public/images/shoes.jpg"
+    image:"/images/shoes.jpg"
   },
   {
     _id: "5",
@@ -75,39 +75,39 @@ export const exclusiveCollection = [
     title:"Italian Leather Loafers",
     price:"199.99",
     isNew: false,
-    image:"/public/images/shoes.jpg"
+    image:"/images/shoes.jpg"
   },
 ];
 export const brands = [{
   _id: "1",
   title: "Versace",
-  image: "/public/images/versace-removebg-preview.png",
+  image: "/images/versace-removebg-preview.png",
 },
 {
   _id: "2",
   title: "Gucci",
-  image: "/public/images/gucci-removebg-preview.png",
+  image: "/images/gucci-removebg-preview.png",
 },
 {
   _id: "3",
   title: "Prada",
-  image: "/public/images/prada-removebg-preview.png",
+  image: "/images/prada-removebg-preview.png",
 },
 {
   _id: "4",
   title: "Dior",
-  image: "/public/images/dior-removebg-preview.png",
+  image: "/images/dior-removebg-preview.png",
 },
 {
   _id: "5",
   title: "Chanel",
-  image: "/public/images/chanel-removebg-preview.png",
+  image: "/images/chanel-removebg-preview.png",
 },
 {
   _id: "6",
 
   title: "Louis Vuitton",
-  image: "/public/images/louis_vuitton-removebg-preview.png",
+  image: "/images/louis_vuitton-removebg-preview.png",
 }];
 export const saleItems = [
   {
@@ -116,7 +116,7 @@ export const saleItems = [
     title:"Premium Cotton Shirt",
     price:"89.99",
     isOnSale: true,
-    image:"/public/images/shirt.jpeg",
+    image:"/images/shirt.jpeg",
     discountPercentage: 20,
     originalPrice: 109.99
   },
@@ -126,7 +126,7 @@ export const saleItems = [
     title:"Designer Slim-Fit Jeans",
     price:"129.99",
     isOnSale: true,
-    image:"/public/images/jeans.jpg",
+    image:"/images/jeans.jpg",
     discountPercentage: 10,
     originalPrice: 149.99
   },
@@ -136,7 +136,7 @@ export const saleItems = [
     title:"Merino Wool Sweater",
     price:"159.99",
     isOnSale: true,
-    image:"/public/images/sweater.jpeg",
+    image:"/images/sweater.jpeg",
     discountPercentage: 15,
     originalPrice: 179.99
   },
@@ -146,7 +146,7 @@ export const saleItems = [
     title:"Italian Leather Loafers",
     price:"199.99",
     isOnSale: true,
-    image:"/public/images/shoes.jpg",
+    image:"/images/shoes.jpg",
     discountPercentage: 25,
     originalPrice: 229.99
   },
@@ -156,7 +156,7 @@ export const saleItems = [
     title:"Italian Leather Loafers",
     price:"199.99",
     isOnSale: true,
-    image:"/public/images/shoes.jpg",
+    image:"/images/shoes.jpg",
     discountPercentage: 25,
     originalPrice: 229.99
   }
